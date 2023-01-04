e35e6306fc0cc1e400699a1f6bc3fc56265a3b66
Zev Landau <creativeprojectsny+macbook@gmail.com>
2023-01-04 02:23:14 -0500

    Added dynamic behaviour to app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useState } from 'react'
 
 import questions from '../questions.json'
 
 const Home: NextPage = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+
+  const handlePrevious = () => {
+    const prevQuestion = currentQuestion - 1;
+    prevQuestion >= 0 && setCurrentQuestion(prevQuestion)
+  }
+
+  const handleNext = () => {
+    const nextQuestion = currentQuestion + 1;
+    nextQuestion < questions.length && setCurrentQuestion(nextQuestion)
+  }
+
   return (
     <div className="flex flex-col w-screen px-5 h-screen bg-[#1A1A1A] justify-center items-center">
       <Head>
@@ -12,15 +25,15 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="flex flex-col items-start w-full">
-        <h4 className="mt-10 text-xl text-white/60">Question 1 of 5</h4>
+        <h4 className="mt-10 text-xl text-white/60">Question {currentQuestion + 1} of {questions.length}</h4>
 
         <div className="mt-4 text-2xl text-white">
-          What type of framework is next.js?
+          {questions[currentQuestion].question}
         </div>
       </div>
 
       <div className="flex flex-col w-full">
-        {questions[0].answerOptions.map((answer, index) => (
+        {questions[currentQuestion].answerOptions.map((answer, index) => (
           <div
             key={index}
             className="flex items-center w-full py-4 pl-5 m-2 ml-0 space-x-2 border-2 cursor-pointer bg-white/5 border-white/10 rounded-xl"
@@ -32,10 +45,10 @@ const Home: NextPage = () => {
       </div>
 
       <div className="flex justify-between w-full mt-4 text-white">
-        <button className="w-[49%] py-3 bg-indigo-600 rounded-lg">
+        <button className="w-[49%] py-3 bg-indigo-600 rounded-lg" onClick={handlePrevious}>
           Previous
         </button>
-        <button className="w-[49%] py-3 bg-indigo-600 rounded-lg">Next</button>
+        <button className="w-[49%] py-3 bg-indigo-600 rounded-lg" onClick={handleNext}>Next</button>
       </div>
     </div>
   )
